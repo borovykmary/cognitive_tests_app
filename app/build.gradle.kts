9ac158e66605003d45793601009dddddce45529a
illia_pastushok <276487@student.pwr.edu.pl>
2024-06-14 10:37:27 +0200

    MiniAce update, db switched to Cloud FireStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/cognittiveassesmenttests/mongoDB/users/UsersDAO.kt
+++ app/src/main/java/com/example/cognittiveassesmenttests/mongoDB/users/UsersDAO.kt
@@ -5,9 +5,5 @@ import com.example.cognittiveassesmenttests.mongoDB.model.User
 
 
 interface UsersDAO {
-
-    //Mongo Realm Endpoint
     suspend fun insertUser(user: User)
-    //MongoDB Driver
-    //suspend fun insertUser(database: MongoDatabase, user: User)
 }

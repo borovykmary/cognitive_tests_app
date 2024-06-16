c61a6f3d604f297897eb11c7e4387d3dd400ad02
illia_pastushok <276487@student.pwr.edu.pl>
2024-06-16 13:10:39 +0200

    Shuffle and indexing for cards impemented, condition by color is set

--- app/src/main/java/com/example/cognittiveassesmenttests/cardsTest/CardsActivity.kt
+++ app/src/main/java/com/example/cognittiveassesmenttests/cardsTest/CardsActivity.kt
@@ -4,6 +4,7 @@ import android.content.ClipData
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.DragEvent
 import android.view.View
 import android.widget.ImageView
@@ -22,6 +23,16 @@ import com.example.cognittiveassesmenttests.R
 //import androidx.activity.compose.setContent
 
 class CardsActivity : AppCompatActivity() {
+    private var dragCounter = 256 // 4 times more than needed, actually it is 64 times
+
+    private fun extractPropertiesFromResourceName(resourceName: String): CardUiItem {
+        val properties = resourceName.split("_")
+        val color = properties[1]
+        val number = properties[2].toInt()
+        val shape = properties[3]
+        return CardUiItem(resourceName, color, number, shape)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -51,6 +62,31 @@ class CardsActivity : AppCompatActivity() {
         // Start the timer
         handler.postDelayed(runnable, 1000)
 
+        val drawablesBlue = listOf(
+            R.drawable.card_blue_1_circle,
+            R.drawable.card_blue_3_circle,
+            R.drawable.card_blue_4_star
+        )
+
+        val drawablesGreen = listOf(
+            R.drawable.card_green_1_triangle,
+            R.drawable.card_green_2_square,
+            R.drawable.card_green_2_triangle
+        )
+
+        val drawablesRed = listOf(
+            R.drawable.card_red_1_triangle,
+            R.drawable.card_red_2_square,
+            R.drawable.card_red_3_star
+        )
+
+        val drawablesYellow = listOf(
+            R.drawable.card_yellow_3_circle,
+            R.drawable.card_yellow_4_square,
+            R.drawable.card_yellow_4_star
+        )
+
+        val answerDrawableIds = drawablesBlue + drawablesGreen + drawablesRed + drawablesYellow
 
         val imageViewAnswer = findViewById<ImageView>(R.id.imageViewAnswer)
         val targets = arrayOf(
@@ -59,6 +95,26 @@ class CardsActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.imageViewTarget3),
             findViewById<ImageView>(R.id.imageViewTarget4)
         )
+
+        val targetDrawableIds = listOf(
+            R.drawable.card_blue_1_circle,
+            R.drawable.card_green_2_square,
+            R.drawable.card_red_3_star,
+            R.drawable.card_yellow_4_star
+        )
+        var answerDrawableId = answerDrawableIds.random()
+        // Set the drawable for the imageViewAnswer
+        imageViewAnswer.setImageResource(answerDrawableId)
+
+        // Set the drawables for the targets
+        targets.forEachIndexed { index, target ->
+            target.setImageResource(targetDrawableIds[index])
+        }
+
+        // Create a CardUiItem for each target
+        val targetItems = targetDrawableIds.map { drawableId ->
+            extractPropertiesFromResourceName(resources.getResourceEntryName(drawableId))
+        }
 
         imageViewAnswer.setOnLongClickListener { v ->
             val clipData = ClipData.newPlainText("", "")
@@ -69,7 +125,11 @@ class CardsActivity : AppCompatActivity() {
             true
         }
 
-        targets.forEach { target ->
+
+        targets.forEachIndexed {index, target ->
+            target.setImageResource(targetDrawableIds[index])
+            // Create a CardUiItem for the current target
+            val currentItem = targetItems[index]
             target.setOnDragListener { v, event ->
                 when (event.action) {
                     DragEvent.ACTION_DRAG_STARTED -> {
@@ -85,16 +145,54 @@ class CardsActivity : AppCompatActivity() {
                         true
                     }
                     DragEvent.ACTION_DROP -> {
+
+                        Log.d("CardsActivity", "AnswerImageId: " + answerDrawableId)
+                        Log.d("CardsActivity", "AnswerImageResource: " + resources.getResourceEntryName(answerDrawableId))
+                        // Now you can use answerDrawableId to get the resource name
+                        val answerItem = extractPropertiesFromResourceName(resources.getResourceEntryName(answerDrawableId))
+                        Log.d("CardsActivity", "AnswerImage: " + answerItem.color)
+                        Log.d("CardsActivity", "CurrentImage: " + currentItem.color)
                         // Check if the item being dropped is the imageViewAnswer
                         if (event.localState === imageViewAnswer) {
-                            Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show()
+                            // Check if the properties match
+                            if (answerItem.color == currentItem.color) {
+                                Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show()
+                            }
                         }
+                        answerDrawableId = answerDrawableIds.random()
+                        // Assign a new drawable resource to imageViewAnswer
+                        imageViewAnswer.setImageResource(answerDrawableId)
                         true
                     }
+
                     DragEvent.ACTION_DRAG_ENDED -> {
-                        // Revert any remaining style changes
-                        imageViewAnswer.scaleX = 1f
-                        imageViewAnswer.scaleY = 1f
+                        dragCounter--
+                        // Check if the drag counter has reached 0
+                        if (dragCounter == 0) {
+                            // Display a toast message
+                            Toast.makeText(this, "No more drags allowed", Toast.LENGTH_SHORT).show()
+
+                            // Disable further dragging
+                            imageViewAnswer.setOnLongClickListener(null)
+                        } else {
+                            // Revert any remaining style changes
+                            imageViewAnswer.scaleX = 1f
+                            imageViewAnswer.scaleY = 1f
+
+                            // Shuffle the lists of drawable resources
+                            val shuffledDrawablesBlue = drawablesBlue.shuffled()
+                            val shuffledDrawablesGreen = drawablesGreen.shuffled()
+                            val shuffledDrawablesRed = drawablesRed.shuffled()
+                            val shuffledDrawablesYellow = drawablesYellow.shuffled()
+
+                            // Assign the shuffled drawable resources to the targets
+                            targets[0].setImageResource(shuffledDrawablesBlue[0])
+                            targets[1].setImageResource(shuffledDrawablesGreen[0])
+                            targets[2].setImageResource(shuffledDrawablesRed[0])
+                            targets[3].setImageResource(shuffledDrawablesYellow[0])
+                        }
                         true
                     }
                     else -> false

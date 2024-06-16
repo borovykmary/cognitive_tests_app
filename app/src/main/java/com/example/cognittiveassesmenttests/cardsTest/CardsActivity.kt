caf21ee5ee2bfdac04369ed93785b4e47a9f59f6
illia_pastushok <276487@student.pwr.edu.pl>
2024-06-16 14:05:50 +0200

    Cards improved, still need to improve shuffle

--- app/src/main/java/com/example/cognittiveassesmenttests/cardsTest/CardsActivity.kt
+++ app/src/main/java/com/example/cognittiveassesmenttests/cardsTest/CardsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cognittiveassesmenttests.cardsTest
 
 import android.content.ClipData
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -19,11 +20,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color*/
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cognittiveassesmenttests.MainActivity
 import com.example.cognittiveassesmenttests.R
 //import androidx.activity.compose.setContent
 
 class CardsActivity : AppCompatActivity() {
-    private var dragCounter = 256 // 4 times more than needed, actually it is 64 times
+    private var dragCounter = 32 // 4 times more than needed, actually it is 64 times
+    private var correctAnswersInRow = 0
+    private var currentCondition = "color"
+    private var correctAnswers = 0
+    private var secondsElapsed = 0
 
     private fun extractPropertiesFromResourceName(resourceName: String): CardUiItem {
         val properties = resourceName.split("_")
@@ -96,7 +102,7 @@ class CardsActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.imageViewTarget4)
         )
 
-        val targetDrawableIds = listOf(
+        var targetDrawableIds = listOf(
             R.drawable.card_blue_1_circle,
             R.drawable.card_green_2_square,
             R.drawable.card_red_3_star,
@@ -112,7 +118,7 @@ class CardsActivity : AppCompatActivity() {
         }
 
         // Create a CardUiItem for each target
-        val targetItems = targetDrawableIds.map { drawableId ->
+        var targetItems = targetDrawableIds.map { drawableId ->
             extractPropertiesFromResourceName(resources.getResourceEntryName(drawableId))
         }
 
@@ -127,9 +133,7 @@ class CardsActivity : AppCompatActivity() {
 
 
         targets.forEachIndexed {index, target ->
-            target.setImageResource(targetDrawableIds[index])
             // Create a CardUiItem for the current target
-            val currentItem = targetItems[index]
             target.setOnDragListener { v, event ->
                 when (event.action) {
                     DragEvent.ACTION_DRAG_STARTED -> {
@@ -145,25 +149,76 @@ class CardsActivity : AppCompatActivity() {
                         true
                     }
                     DragEvent.ACTION_DROP -> {
-
-                        Log.d("CardsActivity", "AnswerImageId: " + answerDrawableId)
-                        Log.d("CardsActivity", "AnswerImageResource: " + resources.getResourceEntryName(answerDrawableId))
+                        val currentItem = targetItems[index]
                         // Now you can use answerDrawableId to get the resource name
                         val answerItem = extractPropertiesFromResourceName(resources.getResourceEntryName(answerDrawableId))
-                        Log.d("CardsActivity", "AnswerImage: " + answerItem.color)
-                        Log.d("CardsActivity", "CurrentImage: " + currentItem.color)
                         // Check if the item being dropped is the imageViewAnswer
                         if (event.localState === imageViewAnswer) {
-                            // Check if the properties match
-                            if (answerItem.color == currentItem.color) {
+                            val isCorrect = when (currentCondition) {
+                                "color" -> answerItem.color == currentItem.color
+                                "shape" -> answerItem.shape == currentItem.shape
+                                "number" -> answerItem.number == currentItem.number
+                                else -> false
+                            }
+
+                            if (isCorrect) {
+                                correctAnswersInRow++
+                                correctAnswers++
                                 Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show()
                             } else {
+                                correctAnswersInRow = 0
                                 Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show()
                             }
+
+                            // If 10 correct answers in a row, randomly change the condition
+                            if (correctAnswersInRow == 10) {
+                                currentCondition = when (currentCondition) {
+                                    "color" -> if (Math.random() < 0.5) "shape" else "number"
+                                    "shape" -> if (Math.random() < 0.5) "color" else "number"
+                                    "number" -> if (Math.random() < 0.5) "color" else "shape"
+                                    else -> currentCondition
+                                }
+                                correctAnswersInRow = 0
+                            }
+                            Log.d("CardsActivity", "AnswerImageId: " + answerDrawableId)
+                            Log.d("CardsActivity", "AnswerImageResource: " + resources.getResourceEntryName(answerDrawableId))
+                            Log.d("CardsActivity", "CurrentImageResource: " + currentItem.resource)
+                            Log.d("CardsActivity", "AnswerImageColor: " + answerItem.color)
+                            Log.d("CardsActivity", "CurrentImageColor: " + currentItem.color)
+                            Log.d("CardsActivity", "AnswerImageShape: " + answerItem.shape)
+                            Log.d("CardsActivity", "CurrentImageShape: " + currentItem.shape)
+                            Log.d("CardsActivity", "AnswerImageNumber: " + answerItem.number)
+                            Log.d("CardsActivity", "CurrentImageNumber: " + currentItem.number)
+                            Log.d("CardsActivity", "Condition: " + currentCondition)
+                            Log.d("CardsActivity", "Correctinrow: " + correctAnswersInRow)
+
                         }
                         answerDrawableId = answerDrawableIds.random()
+                        // Shuffle the lists of drawable resources
+                        val shuffledDrawablesBlue = drawablesBlue.random()
+                        val shuffledDrawablesGreen = drawablesGreen.random()
+                        val shuffledDrawablesRed = drawablesRed.random()
+                        val shuffledDrawablesYellow = drawablesYellow.random()
                         // Assign a new drawable resource to imageViewAnswer
                         imageViewAnswer.setImageResource(answerDrawableId)
+                        // Assign the shuffled drawable resources to the targets
+                        targets[0].setImageResource(shuffledDrawablesBlue)
+                        targets[1].setImageResource(shuffledDrawablesGreen)
+                        targets[2].setImageResource(shuffledDrawablesRed)
+                        targets[3].setImageResource(shuffledDrawablesYellow)
+
+                        targetDrawableIds = listOf(
+                            shuffledDrawablesBlue,
+                            shuffledDrawablesGreen,
+                            shuffledDrawablesRed,
+                            shuffledDrawablesYellow
+                        )
+
+                        // Create a CardUiItem for each target
+                        targetItems = targetDrawableIds.map { drawableId ->
+                            extractPropertiesFromResourceName(resources.getResourceEntryName(drawableId))
+                        }
+
                         true
                     }
 
@@ -171,27 +226,20 @@ class CardsActivity : AppCompatActivity() {
                         dragCounter--
                         // Check if the drag counter has reached 0
                         if (dragCounter == 0) {
-                            // Display a toast message
-                            Toast.makeText(this, "No more drags allowed", Toast.LENGTH_SHORT).show()
+                            // Display a toast message with correct answers and elapsed time
+                            Toast.makeText(this, "No more drags allowed. Correct answers: $correctAnswers. Time: ${textViewTime.text}", Toast.LENGTH_SHORT).show()
 
                             // Disable further dragging
                             imageViewAnswer.setOnLongClickListener(null)
+                            // Create an intent to start MainActivity
+                            val intent = Intent(this, MainActivity::class.java)
+                            // Start MainActivity
+                            startActivity(intent)
                         } else {
                             // Revert any remaining style changes
                             imageViewAnswer.scaleX = 1f
                             imageViewAnswer.scaleY = 1f
 
-                            // Shuffle the lists of drawable resources
-                            val shuffledDrawablesBlue = drawablesBlue.shuffled()
-                            val shuffledDrawablesGreen = drawablesGreen.shuffled()
-                            val shuffledDrawablesRed = drawablesRed.shuffled()
-                            val shuffledDrawablesYellow = drawablesYellow.shuffled()
-
-                            // Assign the shuffled drawable resources to the targets
-                            targets[0].setImageResource(shuffledDrawablesBlue[0])
-                            targets[1].setImageResource(shuffledDrawablesGreen[0])
-                            targets[2].setImageResource(shuffledDrawablesRed[0])
-                            targets[3].setImageResource(shuffledDrawablesYellow[0])
                         }
                         true
                     }

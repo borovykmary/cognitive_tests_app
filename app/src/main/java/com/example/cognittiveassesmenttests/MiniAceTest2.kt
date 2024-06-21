4879608a34482cc2da422f7efba5e4de55a313af
illia_pastushok <276487@student.pwr.edu.pl>
2024-06-21 12:52:14 +0200

    audio shuffle added

--- app/src/main/java/com/example/cognittiveassesmenttests/MiniAceTest2.kt
+++ app/src/main/java/com/example/cognittiveassesmenttests/MiniAceTest2.kt
@@ -1,5 +1,6 @@
 package com.example.cognittiveassesmenttests
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -28,22 +29,35 @@ class MiniAceTest2 : Fragment() {
         return inflater.inflate(R.layout.fragment_mini_ace_test2, container, false)
     }
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    mediaPlayer = MediaPlayer.create(requireContext(), R.raw.voice_message)
+    mediaPlayer = MediaPlayer.create(requireContext(), R.raw.voice_message0)
         var counter: Int = 0
+        var mediaCounter = 0
+        val randomValue = (0..1).random()
 
     val playButton = view.findViewById<ImageView>(R.id.play_button)
 
             playButton.setOnClickListener {
-                if (counter < 2) {
+                if (counter < 3) {
+
                     if (!mediaPlayer.isPlaying) {
                         mediaPlayer.start()
                         counter++
                     } else {
                         mediaPlayer.stop()
                         mediaPlayer.reset()
-                        mediaPlayer = MediaPlayer.create(requireContext(), R.raw.voice_message)
+                        if(mediaCounter == 0) {
+                            mediaPlayer = MediaPlayer.create(requireContext(), R.raw.voice_message_start)
+                            mediaCounter++
+                        } else {
+                            if(randomValue < 0.5) {
+                                mediaPlayer = MediaPlayer.create(requireContext(), R.raw.voice_message0)
+                            } else {
+                                mediaPlayer = MediaPlayer.create(requireContext(), R.raw.voice_message1)
+                            }
+                        }
                     }
                 } else {
                     Toast.makeText(requireContext(), "You cannot listen to audio anymore", Toast.LENGTH_SHORT).show()
